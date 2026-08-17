dc2af005eb4e239ce4ae2ac3d5d816db16dd0fa9
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-08-17 05:00:03 +0000

    add books page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
 import Dashboard from "./pages/Dashboard";
+import Books from "./pages/Books";
 
 function App() {
-  return <Dashboard />;
+  const [page, setPage] = useState("Dashboard");
+
+  if (page === "Books") {
+    return <Books />;
+  }
+
+  return (
+    <DashboardWithNavigation
+      onNavigate={setPage}
+    />
+  );
+}
+
+type DashboardWithNavigationProps = {
+  onNavigate: (page: string) => void;
+};
+
+function DashboardWithNavigation({
+  onNavigate,
+}: DashboardWithNavigationProps) {
+  return (
+    <DashboardNavigationBridge onNavigate={onNavigate} />
+  );
+}
+
+function DashboardNavigationBridge({
+  onNavigate,
+}: DashboardWithNavigationProps) {
+  return <Dashboard onNavigate={onNavigate} />;
 }
 
 export default App;
